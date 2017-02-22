72431b8b936834a6a5c435488f5eb705ba52c691
timearchitect <alrik_he@hotmail.com>
2017-02-22 08:57:42 +0100

    timer text change to mm:ss

--- Assets/scripts/main/timer.js
+++ Assets/scripts/main/timer.js
@@ -1,10 +1,12 @@
 ﻿#pragma strict
 
 var timer  : float = 90.0;
-
+var minutes : int =0;
+var seconds : int =0;
 function Update () {
 	timer -= Time.deltaTime;
-
+	 minutes = Mathf.Floor(timer / 60);
+	 seconds = Mathf.Ceil(timer % 60);
 	if(timer <= 0){
 	 timer = 0;
 	 // Förlust
@@ -12,6 +14,7 @@ function Update () {
 }
 
 function OnGUI(){
- 	GUI.Box(new Rect(10, 10, 50, 20), "" + timer.ToString("0"));
+//"" + timer.ToString("0")
+ 	GUI.Box(new Rect(10, 10, 50, 20), " "+ minutes.ToString("00") +":"+seconds.ToString("00") );
  	
 }
