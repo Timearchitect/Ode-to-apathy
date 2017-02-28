52f2d80fafa5ced4b020b74ad6360ab41c4cc6eb
timearchitect <alrik_he@hotmail.com>
2017-02-28 10:43:22 +0100

    result and merge

--- Assets/scripts/main/result.cs
+++ Assets/scripts/main/result.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class result : MonoBehaviour {
 	GameObject review ;
-
 
 	// Use this for initialization
 	void Start () {
 		review = GameObject.Find("ReviewPanel");
 		print (review.name+"!!!!!!!!!!!!!!!!!!!!!");
-		if (review != null) {
-			if (review.GetComponent<UnityEngine.UI.Text> () != null) print ("NOT NULL text");
-			else print (" NULL text");
 
+		if (review != null) {
+			//print (review.guiText);
+			if (review.GetComponent<UnityEngine.UI.Text> ().text != null) print ("NOT NULL text");
+			else print (" NULL text");
 			foreach (Component e in review.GetComponents<Component>()) {
 				print (e.name +"  "+ e.ToString());
 			}
- 
+
 			//review.GetComponent <Text> ().text = "  Dummy Text  ";
 		}
 	}
