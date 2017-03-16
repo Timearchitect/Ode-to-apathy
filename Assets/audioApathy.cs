14dff90cadabe1a255fff70c550f7b35fea30507
Mattias Wendt <mattias.wendt@gmail.com>
2017-03-16 23:00:58 +0100

    to the limit

---
+++ Assets/audioApathy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class audioApathy : MonoBehaviour {
+
+	public int level;
+	public GameObject go;
+	public AudioSource apathyAudio;
+	public AudioClip apathyClip;
+
+	// Use this for initialization
+	void Start () {
+		level = Stats.currentLevel;
+	//	go = GameObject.FindGameObjectWithTag ("endCard");
+		apathyAudio = GetComponent<AudioSource> ();
+
+		apathyClip = Resources.Load ("apathy_ending_"+level,typeof(AudioClip)) as AudioClip;
+
+
+		apathyAudio.clip = apathyClip;
+		apathyAudio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
