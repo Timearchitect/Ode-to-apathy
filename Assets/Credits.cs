57f347288b0645a431d95b8d0cb1367495aba854
Graziella Sundblad <aluminiummonster@graziellas-mbp.clients.internal.mah.se>
2017-03-24 13:50:54 +0100

    credits

---
+++ Assets/Credits.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+	private float offset;
+	public float speed = 50.0f;
+	public GUIStyle style;
+	public Rect viewArea;
+
+	private void Start()
+	{
+		this.offset = this.viewArea.height;
+	}
+
+	private void Update()
+	{
+		this.offset -= Time.deltaTime * this.speed;
+	}
+
+	private void OnGUI()
+	{
+		GUI.BeginGroup(this.viewArea);
+
+		var position = new Rect(0, this.offset, this.viewArea.width, this.viewArea.height);
+		var text = @"Ode to Apathy - the best game ever, brought to you by:
+		
+Alrik He
+Graziella Sundblad
+Jakob Håkonsson
+Mattias Wendt 
+Victor Lind
+
+
+";
+
+		GUI.Label(position, text, this.style);
+
+		GUI.EndGroup();
+	}
+}
